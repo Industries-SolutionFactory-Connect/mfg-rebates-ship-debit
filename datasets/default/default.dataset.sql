5f190a63eef6d54b6a181bcb65dc777643077643
Meghana Ksheersagar <mksheersagar@salesforce.com>
2023-06-07 15:37:08 +0530

    rename duplicate account record

--- datasets/default/default.dataset.sql
+++ datasets/default/default.dataset.sql
@@ -38,7 +38,7 @@ CREATE TABLE "Account" (
 	"ParentId" VARCHAR(255), 
 	PRIMARY KEY (id)
 );
-INSERT INTO "Account" VALUES(1,'','(212) 555-5555','Acme','','(212) 555-5555','','','','Prospect','100000000.0','New York','USA','','','','NY','10 Main Rd.','31349','','680','Manufacturing','','New York','USA','','','','NY','10 Main Rd.','31349','','','','','');
+INSERT INTO "Account" VALUES(1,'','(212) 555-5555','Acme_New','','(212) 555-5555','','','','Prospect','100000000.0','New York','USA','','','','NY','10 Main Rd.','31349','','680','Manufacturing','','New York','USA','','','','NY','10 Main Rd.','31349','','','','','');
 INSERT INTO "Account" VALUES(4,'','','Analog devices','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','');
 INSERT INTO "Account" VALUES(5,'','','Dreamer','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','');
 INSERT INTO "Account" VALUES(6,'','','DreamWaver','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','');
